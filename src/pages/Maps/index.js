da51d022c62436cb01e45d0fbee43bca6151f4a4
William Teixeira <williderteixeira@gmail.com>
2022-07-14 13:10:08 -0400

    feat: add Geolocation lib

--- src/pages/Maps/index.js
+++ src/pages/Maps/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import {StyleSheet, View, Dimensions} from 'react-native';
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import Geolocation from '@react-native-community/geolocation';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Text,
+  TouchableOpacity,
+  PermissionsAndroid,
+} from 'react-native';
 
 import MapView, {Marker} from 'react-native-maps';
 
@@ -11,191 +20,171 @@ const LONGITUDE = -60.01716;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-const customStyle = [
-  {
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#242f3e',
-      },
-    ],
-  },
-  {
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#746855',
-      },
-    ],
-  },
-  {
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#242f3e',
-      },
-    ],
-  },
-  {
-    featureType: 'administrative.locality',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#d59563',
-      },
-    ],
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#d59563',
-      },
-    ],
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#263c3f',
-      },
-    ],
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#6b9a76',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#38414e',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.stroke',
-    stylers: [
-      {
-        color: '#212a37',
-      },
-    ],
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#9ca5b3',
-      },
-    ],
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#746855',
-      },
-    ],
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [
-      {
-        color: '#1f2835',
-      },
-    ],
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#f3d19c',
-      },
-    ],
-  },
-  {
-    featureType: 'transit',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#2f3948',
-      },
-    ],
-  },
-  {
-    featureType: 'transit.station',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#d59563',
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#17263c',
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#515c6d',
-      },
-    ],
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#17263c',
-      },
-    ],
-  },
-];
+const GEOLOCATION_OPTIONS = {
+  enableHighAccuracy: true,
+  timeout: 20000,
+  maximumAge: 1000,
+};
+const ANCHOR = {x: 0.5, y: 0.5};
 
-export default function MapScreen() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map}>
-        <Marker
-          title="EST"
-          description="UEA"
-          coordinate={{
+const colorOfmyLocationMapMarker = 'blue';
+
+const propTypes = {
+  ...Marker.propTypes,
+  // override this prop to make it optional
+  coordinate: PropTypes.shape({
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+  }),
+  children: PropTypes.node,
+  geolocationOptions: PropTypes.shape({
+    enableHighAccuracy: PropTypes.bool,
+    timeout: PropTypes.number,
+    maximumAge: PropTypes.number,
+  }),
+  heading: PropTypes.number,
+  enableHack: PropTypes.bool,
+};
+
+const defaultProps = {
+  enableHack: false,
+  geolocationOptions: GEOLOCATION_OPTIONS,
+};
+
+export default class MapScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: -3.0917797,
+      long: -60.0175282,
+      myPosition: null,
+    };
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    if (this.props.coordinate) {
+      return;
+    }
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permissão de Acesso à Localização',
+          message: 'Este aplicativo precisa acessar sua localização.',
+          buttonNeutral: 'Pergunte-me depois',
+          buttonNegative: 'Cancelar',
+          buttonPositive: 'OK',
+        },
+      ).then(granted => {
+        if (granted && this.mounted) {
+          this.watchLocation();
+        }
+      });
+    } else {
+      this.watchLocation();
+    }
+  }
+
+  watchLocation() {
+    Geolocation.getCurrentPosition(
+      position => {
+        this.state.myPosition = position.coords;
+      },
+      error => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    );
+    this.watchID = Geolocation.watchPosition(position => {
+      const myLastPosition = this.state.myPosition;
+      const myPosition = position.coords;
+      if (!(myPosition === myLastPosition)) {
+        //aqui
+        this.setState({myPosition});
+      }
+    });
+  }
+
+  clearLocation = () => {
+    Geolocation.clearWatch(this.watchID);
+  };
+
+  componentWillUnmount() {
+    this.mounted = false;
+    if (this.watchID) {
+      clearLocation();
+    }
+  }
+
+  render() {
+    let {heading, coordinate} = this.props;
+    if (!coordinate) {
+      const {myPosition} = this.state;
+      if (!myPosition) {
+        return null;
+      }
+      coordinate = myPosition;
+      heading = myPosition.heading;
+    }
+
+    const rotate =
+      typeof heading === 'number' && heading >= 0 ? `${heading}deg` : null;
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
             latitude: LATITUDE,
             longitude: LONGITUDE,
-          }}
-        />
-      </MapView>
-    </View>
-  );
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}>
+          <Marker
+            title="EST"
+            description="UEA"
+            coordinate={{
+              latitude: LATITUDE,
+              longitude: LONGITUDE,
+            }}
+          />
+        </MapView>
+        <View style={styles.subcontainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.showCurrentLocation}>
+            <Text>Mostrar local atual</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>Mostrar EST</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
+    height: '100%',
+  },
+  subcontainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingHorizontal: 10,
+    height: 200,
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+  },
 });
+
+MapScreen.propTypes = propTypes;
+MapScreen.defaultProps = defaultProps;
