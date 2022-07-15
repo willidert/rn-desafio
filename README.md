10d4d6e12a9eae42a2a3bf94ab75b055742cd874
William Teixeira <williderteixeira@gmail.com>
2022-07-14 20:27:55 -0400

    Feat/qrcode (#2)
    * chore: add lib to maps
    
    * chore: update README
    
    * chore: add .editorconfig
    
    * feat:add lib for qrcode and camera
    
    * chore: remove API KEY
    
    * feat: add qrcode
    
    * chore: update package.lock

--- README.md
+++ README.md
@@ -11,6 +11,18 @@ Um app desenvolvido em React-Native para um desafio de seleção de um projeto d
 - Deve conter um botão para enviar email para o dev
 - Deve conter um botão para enviar wpp para o dev
 
+- configurar variavel de ambiente para a chave de API do maps
+
 ### Problemas
 
-- configurar variavel de ambiente para a chave de API do maps
+- habilitar a API do maps, n consegui verificar minha conta pra usar o serviço
+- o lib que eu uso pra qrcode usa cm dependencia uma lib pra acessar a camera, mas essa lib ta desatualizada
+  ["possivel solução"(https://github.com/facebook/react-native/issues/33557#issuecomment-1100919812), ele n menciona mas precisa instalar essa [deprecated](https://www.npmjs.com/package/deprecated-react-native-prop-types)...
+
+### Executando
+
+```sh
+npx i
+npx react-native run-android
+```
+
--- src/pages/Qrcode/index.js
+++ src/pages/Qrcode/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Linking, Text, TouchableOpacity, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
-export default function QrcodeScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{color: '#000'}}>Ola Qrcode</Text>
-    </View>
-  );
+export default class QrcodeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      link: '',
+    };
+  }
+
+  handleLink = () => {
+    Linking.openURL(this.state.link).catch(() => {
+      console.log('Houve um erro');
+    });
+  };
+  render() {
+    return (
+      <QRCodeScanner
+        onRead={data => {
+          this.setState({link: data.data});
+        }}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        topContent={
+          <View>
+            <Text style={{color: '#000'}}>{this.state.link}</Text>
+          </View>
+        }
+        bottomContent={
+          <View>
+            <TouchableOpacity
+              onPress={this.handleLink}
+              style={{padding: 12, backgroundColor: '#0277ba', marginTop: 10}}>
+              <Text style={{color: '#FFF'}}>Some text</Text>
+            </TouchableOpacity>
+          </View>
+        }
+      />
+    );
+  }
 }
